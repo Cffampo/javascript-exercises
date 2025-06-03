d25ccbab15a30cf90bb2c4a211cf67df247529f2
cffampo <cffampo@gmail.com>
2025-06-02 20:52:17 -0400

    Finished exercises 6-11

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,4 +1,12 @@
-const reverseString = function() {
+const reverseString = function(word) {
+
+    result = "";
+
+    for(let i = word.length - 1; i >= 0; i--){
+        result += word.charAt(i);
+    }
+
+    return result;
 
 };
 
--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,26 @@
-const sumAll = function() {
+const sumAll = function(startNumber, endNumber) {
+
+    if(startNumber < 0|| endNumber < 0){
+        return "ERROR";
+    }
+
+    if (!Number.isInteger(startNumber) || !Number.isInteger(endNumber)) {
+    return "ERROR";
+
+    }
+
+    if (startNumber > endNumber) {
+    [startNumber, endNumber] = [endNumber, startNumber];
+}
+
+
+    let sum = 0;
+
+    for(let i = startNumber; i <= endNumber; i++){
+        sum += i;
+    }
+
+    return sum;
 
 };
 
